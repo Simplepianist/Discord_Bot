9ece9f0fcd1158df52bf99766806aef04874f9d3
Simplebox <dev@simplebox.dev>
2025-02-02 17:56:52 +0100

    Website-API: * make static method

--- API/content_api.py
+++ API/content_api.py
@@ -28,42 +28,42 @@ class API:
         Initializes the API class with bot and database instances.
         """
         self.router = APIRouter()
-        self.router.add_api_route("/scoreboard", self.scoreboard, methods=["GET"])
-        self.router.add_api_route("/commands", self.command_list, methods=["GET"])
+        self.router.add_api_route("/scoreboard", scoreboard, methods=["GET"])
+        self.router.add_api_route("/commands", command_list, methods=["GET"])
 
-    async def scoreboard(self, request: Request):
-        """
-        Asynchronously retrieves and returns the scoreboard data.
+async def scoreboard(request: Request):
+    """
+    Asynchronously retrieves and returns the scoreboard data.
 
-        Returns:
-            A list of dictionaries containing usernames and their corresponding money.
-        """
-        db = request.app.state.db
-        bot = request.app.state.bot
-        scorelist = sorted(await db.get_users_with_money(), key=lambda x: x[1], reverse=True)
-        result = []
-        for score in scorelist:
-            user = bot.get_user(int(score[0])) or await bot.fetch_user(int(score[0]))
-            result.append({"username": user.name, "money": score[1]})
-        return result
+    Returns:
+        A list of dictionaries containing usernames and their corresponding money.
+    """
+    db = request.app.state.db
+    bot = request.app.state.bot
+    scorelist = sorted(await db.get_users_with_money(), key=lambda x: x[1], reverse=True)
+    result = []
+    for score in scorelist:
+        user = bot.get_user(int(score[0])) or await bot.fetch_user(int(score[0]))
+        result.append({"username": user.name, "money": score[1]})
+    return result
 
-    async def command_list(self, request: Request):
-        """
-        Asynchronously retrieves and returns the list of commands.
+async def command_list(self, request: Request):
+    """
+    Asynchronously retrieves and returns the list of commands.
 
-        Returns:
-            A list of dictionaries containing cog names and their corresponding commands.
-        """
-        bot = request.app.state.bot
-        result = []
-        for cog_name, cog in bot.cogs.items():
-            commands = []
-            for command in cog.get_commands():
-                if isinstance(command, Command):
-                    checks = [check.__qualname__.split(".")[0] for check in command.checks]
-                    aliases = []
-                    for alias in command.aliases:
-                        aliases.append("." + alias)
-                    commands.append([command.name, aliases, command.description, checks])
-            result.append({cog_name: commands})
-        return result
+    Returns:
+        A list of dictionaries containing cog names and their corresponding commands.
+    """
+    bot = request.app.state.bot
+    result = []
+    for cog_name, cog in bot.cogs.items():
+        commands = []
+        for command in cog.get_commands():
+            if isinstance(command, Command):
+                checks = [check.__qualname__.split(".")[0] for check in command.checks]
+                aliases = []
+                for alias in command.aliases:
+                    aliases.append("." + alias)
+                commands.append([command.name, aliases, command.description, checks])
+        result.append({cog_name: commands})
+    return result
